907235ce67ac20ba7ff57d23776bd136eeeda132
Eugene Rozhkov <NodCaleb@Teiwaz.local>
2015-02-17 18:55:50 +0300

    Initial Commit

---
+++ iKatalog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iKatalog
+//
+//  Created by Eugene Rozhkov on 17.02.15.
+//  Copyright (c) 2015 Nord Point. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
